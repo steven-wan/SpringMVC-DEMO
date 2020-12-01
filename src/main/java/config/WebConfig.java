06e082ce92b60831522fd5240c15c78062e84307
steven.wan <951520698@qq.com>
2020-12-01 16:25:53 +0800

    config java

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author steven-wan
+ * @desc
+ * @date 2020-12-01 16:25
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+}
